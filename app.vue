55ad290d31b92bf0c0c0e4a4964d87048e60fc21
wencaizhang <1052642137@qq.com>
2024-09-14 09:41:37 +0800

    add strapi module

--- app.vue
+++ app.vue
@@ -4,3 +4,28 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+type Post = {
+  title: string;
+  id: number;
+  like: number;
+  download: boolean;
+}
+
+const { find } = useStrapi<Post>()
+
+// typed to Course
+const response = await find('posts').then(res => {
+  console.log("%c 🥪 res", "font-size:18px;color:#ffffff;background:#ffdd4d", res);
+
+}).catch(err => {
+  console.log("%c 🍯 err", "font-size:18px;color:#ffffff;background:#3f7cff", err);
+
+})
+// console.log("%c 🥑 response", "font-size:18px;color:#ffffff;background:#3f7cff", response);
+</script>
+
+<style scoped>
+
+</style>
